36881b3dda1c85ad203a0a835b91443aaf4aef17
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2022-02-28 18:39:57 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -63,5 +63,103 @@ function smallestCommons(arr) {
 //smallestCommons([2, 10]);
 //smallestCommons([1, 13]);
 smallestCommons([23, 18]);
+
+//return arr.filter(el => func(el)); //works for most
+//console.log(arr.some(el => func(el))); returns true
+//console.log(arr.shift());
+
+function dropElements(arr, func) {
+  const length = arr.length;
+  for (let i = 0; i < length; i++) {
+    if (func(arr[0])) {
+      break;
+    } else {
+      arr.shift();
+    }
+  }
+  return arr;
+}
+
+console.log(
+  dropElements([1, 2, 3, 4], function (n) {
+    return n >= 3;
+  })
+); //[3, 4]
+
+console.log(
+  dropElements([0, 1, 0, 1], function (n) {
+    return n === 1;
+  })
+); //[1, 0, 1]
+console.log(
+  dropElements([1, 2, 3, 9, 2], function (n) {
+    return n > 2;
+  })
+);
+//[3, 9, 2];
 /////////////////////////////////////////////////
+//without using flatMap
+function steamrollArray(arr) {
+  let newArr = [];
+  //console.log(Array.isArray(arr[1]));
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i]) === false && Object.keys(arr[i]).length === 0) {
+      newArr.push(arr[i]);
+    } else if (typeof arr[i] !== 'object') {
+      newArr.push(arr[i]);
+    } else {
+      for (let j = 0; j < arr[i].length; j++) {
+        if (typeof arr[i][j] !== 'object') {
+          newArr.push(arr[i][j]);
+        } else {
+          for (let k = 0; k < arr[i][j].length; k++) {
+            if (typeof arr[i][j][k] !== 'object') {
+              newArr.push(arr[i][j][k]);
+            } else {
+              for (let l = 0; l < arr[i][j][k].length; l++) {
+                if (typeof arr[i][j][k][l] !== 'object') {
+                  newArr.push(arr[i][j][k][l]);
+                } else {
+                  for (let m = 0; m < arr[i][j][k][l].length; m++)
+                    if (typeof arr[i][j][k][l][m] !== 'object') {
+                      newArr.push(arr[i][j][k][l][m]);
+                    } else {
+                      for (let n = 0; n < arr[i][j][k][l][m].length; n++) {
+                        if (typeof arr[i][j][k][l][m][n] !== 'object') {
+                          newArr.push(arr[i][j][k][l][m][n]);
+                        }
+                      }
+                    }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return newArr;
+  //console.log(newArr);
+  //console.log(arr);
+}
+
+//steamrollArray([1, [2], [3, [[4]]]]);
+//steamrollArray([[['a']], [['b']]]);
+steamrollArray([1, [], [3, [[4]]]]); //should return //[1,3,4]
+steamrollArray([1, {}, [3, [[4]]]]);
+
+function binaryAgent(str) {
+  str = str.split(' ');
+  return str.map(el => String.fromCharCode(parseInt(el, 2))).join('');
+}
+
+binaryAgent(
+  '01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111'
+);
+//const newArr = str.split(' ');
+//console.log(newArr);
+//console.log(newArr.charCodeAt(0));
+//const charCode = str.charCodeAt(0);
+//const newStr = String.fromCharCode(charCode);
+//console.log(charCode);
 */
